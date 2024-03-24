6a606a8dceddb74b5f463c94f73ee3f6da872f31
mariuszbee <bemariuszdev@gmail.com>
2024-03-24 22:44:09 +0100

    add aria-label to spinner SpinnerSmall.tsx

--- src/ui/atoms/SpinnerSmall.tsx
+++ src/ui/atoms/SpinnerSmall.tsx
@@ -1,7 +1,10 @@
 export const SpinnerSlow = () => {
 	return (
 		<>
-			<div className="h-6 w-6 animate-spin rounded-full border-8 border-gray-300 border-t-yellow-500"></div>
+			<div
+				className="h-6 w-6 animate-spin rounded-full border-8 border-gray-300 border-t-yellow-500"
+				aria-busy="true"
+			></div>
 		</>
 	);
 };
